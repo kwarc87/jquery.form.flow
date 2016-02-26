c8b9f273e384976c21802f3ed65d0c0f38037817
jakubk@mawelous.com <jakubk@mawelous.com>
2016-02-26 13:40:46 +0100

    Added slider to example form.

--- js/additional-functions.js
+++ js/additional-functions.js
@@ -138,5 +138,199 @@
         }
     }, 'This field is required for home owner.');
 
+    /**
+     * slider
+     */
+
+    var sliderValues = [
+        {
+            "range" : "$0-50",
+            "desc"  : "Congrats! You’re ahead of the game. Lock in your advantage by going solar.",
+            "color" : "light-green"
+        },
+        {
+            "range" : "$51-100",
+            "desc"  : "Nice! Let's see if going solar can reduce your bill even lower.",
+            'color' : "dark-green"
+        },
+        {
+            "range" : "$101-150",
+            "desc"  : "You’re bill is a bit higher than average, but Solar Save could reduce your bill by at least 70%.",
+            "color" : "yellow"
+        },
+        {
+            "range" : "$151-200",
+            "desc"  : "You’re paying a lot for electricity! Solar Save could bring your bill down by at least 70%.",
+            "color" : "orange"
+        },
+        {
+            "range" : "$201-300",
+            "desc"  : "You’re paying a lot for electricity! Solar Save could bring your bill down by at least 70%.",
+            "color" : "orange"
+        },
+        {
+            "range" : "$301-400",
+            "desc"  : "You’re paying a lot for electricity! Solar Save could bring your bill down by at least 70%.",
+            "color" : "orange"
+        },
+        {
+            "range" : "$401-500",
+            "desc"  : "You’re paying a lot for electricity! Solar Save could bring your bill down by at least 70%.",
+            "color" : "orange"
+        },
+        {
+            "range" : "$501-600",
+            "desc"  : "Whoa! That’s a big chunk of change. Solar Save could help you lower this cost at $0 downpayment.",
+            "color" : "red"
+        },
+        {
+            "range" : "$601-700",
+            "desc"  : "Whoa! That’s a big chunk of change. Solar Save could help you lower this cost at $0 downpayment.",
+            "color" : "red"
+        },
+        {
+            "range" : "$701-800",
+            "desc"  : "Whoa! That’s a big chunk of change. Solar Save could help you lower this cost at $0 downpayment.",
+            "color" : "red"
+        },
+        {
+            "range" : "$801",
+            "desc"  : "Whoa! That’s a big chunk of change. Solar Save could help you lower this cost at $0 downpayment.",
+            "color" : "red"
+        }
+    ];
+
+    function sliderRanges(val) {
+        if (val <= 50) {
+            return sliderValues[0]['range'];
+        } else if (val > 50 && val <= 100 ) {
+            return sliderValues[1]['range'];
+        } else if (val > 100 && val <= 150 ) {
+            return sliderValues[2]['range'];
+        } else if (val > 150 && val <= 200 ) {
+            return sliderValues[3]['range'];
+        } else if (val > 200 && val <= 300 ) {
+            return sliderValues[4]['range'];
+        } else if (val > 300 && val <= 400 ) {
+            return sliderValues[5]['range'];
+        } else if (val > 400 && val <= 500 ) {
+            return sliderValues[6]['range'];
+        } else if (val > 500 && val <= 600 ) {
+            return sliderValues[7]['range'];
+        } else if (val > 600 && val <= 700 ) {
+            return sliderValues[8]['range'];
+        } else if (val > 700 && val <= 800 ) {
+            return sliderValues[9]['range'];
+        } else if (val > 800 ) {
+            return sliderValues[10]['range'];
+        }
+    }
+
+    function sliderDescription(val) {
+        switch(val) {
+            case '$0-50':
+                return sliderValues[0]['desc'];
+                break;
+            case '$51-100':
+                return sliderValues[1]['desc'];
+                break;
+            case '$101-150':
+                return sliderValues[2]['desc'];
+                break;
+            case '$151-200':
+                return sliderValues[3]['desc'];
+                break;
+            case '$201-300':
+                return sliderValues[4]['desc'];
+                break;
+            case '$301-400':
+                return sliderValues[5]['desc'];
+                break;
+            case '$401-500':
+                return sliderValues[6]['desc'];
+                break;
+            case '$501-600':
+                return sliderValues[7]['desc'];
+                break;
+            case '$601-700':
+                return sliderValues[8]['desc'];
+                break;
+            case '$701-800':
+                return sliderValues[9]['desc'];
+                break;
+            case '$801':
+                return sliderValues[10]['desc'];
+                break;
+        }
+    }
+
+    function sliderColor(val) {
+        switch(val) {
+            case '$0-50':
+                return sliderValues[0]['color'];
+                break;
+            case '$51-100':
+                return sliderValues[1]['color'];
+                break;
+            case '$101-150':
+                return sliderValues[2]['color'];
+                break;
+            case '$151-200':
+                return sliderValues[3]['color'];
+                break;
+            case '$201-300':
+                return sliderValues[4]['color'];
+                break;
+            case '$301-400':
+                return sliderValues[5]['color'];
+                break;
+            case '$401-500':
+                return sliderValues[6]['color'];
+                break;
+            case '$501-600':
+                return sliderValues[7]['color'];
+                break;
+            case '$601-700':
+                return sliderValues[8]['color'];
+                break;
+            case '$701-800':
+                return sliderValues[9]['color'];
+                break;
+            case '$801':
+                return sliderValues[10]['color'];
+                break;
+        }
+    }
+
+    var slider = document.getElementById('monthly_bill_slider');
+
+    noUiSlider.create(slider, {
+        start: 200,
+        step: 1,
+        tooltips: true,
+        range: {
+            'min': 0,
+            'max': 801
+        },
+        connect: "lower",
+        format: wNumb({
+            decimals: 0,
+            edit: function(value) {
+                return sliderRanges(value);
+            }
+        })
+    });
+
+    var $sliderContainer = $('#power_slider_container');
+    var sliderValueElement = document.getElementById('power_slider_preview');
+
+    slider.noUiSlider.on('update', function( values, handle ) {
+        var newValue = sliderDescription(values[handle]);
+        sliderValueElement.innerHTML = newValue;
+        $sliderContainer.removeClass();
+        $sliderContainer.addClass(sliderColor(values[handle]));
+        $("#monthly_bill").val(values[handle]);
+    });
+
 })();
 
